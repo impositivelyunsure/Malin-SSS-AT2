eb0b8a8f8bb92985c1bfce3b55d88872ab95d4c1
Ethan Daly <M323114@tafe.wa.edu.au>
2025-09-12 12:49:11 +0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Malin_SSS_AT2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcVelocity_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCalcDistance_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCalcTemp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCalcEventHorizon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustTextBoxes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustButtons_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustListBoxes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustBackground_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
